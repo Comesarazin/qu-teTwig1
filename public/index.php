b48a938e53367a2ef255124a520fc220f1691c08
ComeSarazin <sasacoco@orange.fr>
2023-11-09 14:25:40 +0100

    Quête Twig 1 fini

--- public/index.php
+++ public/index.php
@@ -5,5 +5,7 @@ require_once __DIR__ . '/../config/twig.php';
 
 
 $name = 'Wilder';
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
 
 echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('products.html.twig', ['products' => $products]);
